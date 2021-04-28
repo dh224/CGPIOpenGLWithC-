b0a51ec61e28edfa5a87cec3dbeeddf87547e963
dhneverdie <583656269@qq.com>
2021-04-28 19:45:27 +0800

    给立方体插值了不同的颜色

--- OpenGLProject1/fragShader4-1.glsl
+++ OpenGLProject1/fragShader4-1.glsl
@@ -1,9 +1,10 @@
 #version 430
-out vec4 color;
 
+out vec4 color;
+in vec4 varyingColor;
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
 void main(void)
 {
-	color = vec4(1.0, 0.0, 0.0, 1.0);
+	color = varyingColor;
 }
