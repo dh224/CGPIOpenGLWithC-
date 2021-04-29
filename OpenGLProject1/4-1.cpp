8722a0a0f6c576b9e34bf0b21b1d94f3c8a3efba
dhneverdie <583656269@qq.com>
2021-04-29 14:21:45 +0800

    多个模型

--- OpenGLProject1/4-1.cpp
+++ OpenGLProject1/4-1.cpp
@@ -17,21 +17,22 @@ using namespace std;
 
 Utils util = Utils();
 float cameraX, cameraY, cameraZ;
-float cubeLocX, cubeLocY, cubeLocZ;
+float cubeLocX, cubeLocY, cubeLocZ, pyrLocX, pyrLocY, pyrLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 GLuint tfLoc;
 float tf; //time factor
 //分配在display()中所需要的变量的空间，这样就不需要多次分配空间了。
-GLuint mvLoc, projLoc,vLoc;
+GLuint mvLoc, projLoc,vLoc ;
 int width, height;
 float aspect;
 glm::mat4 pMat, vMat, mMat, mvMat,rMat,tMat;// r,tMat用于旋转 动画
 
 
-void setupVertices(void) { //36个顶点，12个三角形，组成了放置在远点的2*2*2的立方体
-	float vertexPositions[108] = {
+void setupVertices(void) { 
+	//36个顶点，12个三角形，组成了放置在远点的2*2*2的立方体
+	float cubePositions[108] = {
 		-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
 		1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
 		1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
@@ -45,26 +46,32 @@ void setupVertices(void) { //36个顶点，12个三角形，组成了放置在远点的2*2*2的立方
 		-1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
 		1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
 	};
-
+	float pyramidPositions[54] =
+	{ -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,    //front
+		1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,    //right
+		1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,  //back
+		-1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,  //left
+		-1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, //LF
+		1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f  //RR
+	};
 	glGenVertexArrays(1, vao);
 	glBindVertexArray(vao[0]);
 	glGenBuffers(numVBOs, vbo);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubePositions), cubePositions, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pyramidPositions), pyramidPositions, GL_STATIC_DRAW);
 }
 
 
 
 
 void init(GLFWwindow* window) {
-	renderingProgram = Utils::createShaderProgram("verShader4-1.glsl", "fragShader4-1.glsl");
-	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 420.0f;
+	renderingProgram = Utils::createShaderProgram("vertShader4-7.glsl", "fragShader4-1.glsl");
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
 	cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
-	glfwGetFramebufferSize(window, &width, &height);
-
-	aspect = (float)width / (float)height;
-	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472为60度的弧度值
+	pyrLocX = 2.0f; pyrLocY = 2.0f; pyrLocZ = 1.0f;
 
 	setupVertices();
 }
@@ -77,20 +84,26 @@ void display(GLFWwindow* window, double currentTime) {
 
 
 	// 获取ModelView矩阵和project矩阵的统一变量
-	//mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
-	vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+
+	//vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
+	glfwGetFramebufferSize(window, &width, &height);
+
+	aspect = (float)width / (float)height;
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472为60度的弧度值
 	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
 	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+	mvMat = vMat * mMat;
 
 
 
 
-	tf = ((float)currentTime);
-	tfLoc = glGetUniformLocation(renderingProgram, "tf");
-	glUniform1f(tfLoc, (float)tf);
+	//tf = ((float)currentTime);
+	//tfLoc = glGetUniformLocation(renderingProgram, "tf");
+	//glUniform1f(tfLoc, (float)tf);
 
-
-	glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
 
 	//将VBO关联到顶点着色器中
@@ -100,7 +113,23 @@ void display(GLFWwindow* window, double currentTime) {
 
 	glEnable(GL_DEPTH_TEST);//调整OpenGL设置，绘制模型
 	glDepthFunc(GL_LEQUAL);
-	glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 100000);	// 0, 36, 24  (or 100000)	
+	glDrawArrays(GL_TRIANGLES, 0, 36);	// 0, 36, 24  (or 100000)	
+
+		// draw the pyramid using buffer #1
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(pyrLocX, pyrLocY, pyrLocZ));
+	mvMat = vMat * mMat;
+
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+	glDrawArrays(GL_TRIANGLES, 0, 18);
 }
 
 
